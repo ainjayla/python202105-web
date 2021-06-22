35005e5743a220dc8d991e26ccb2d1c4d64f796e
Ülari Ainjärv <ylari.ainjarv@gmail.com>
2021-06-22 11:58:07 +0300

    Lisasin REST API funktsionaalsuse.

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from random import randint
+from db import get_database
+from bson.json_util import dumps
 
 app = Flask(__name__)
 
@@ -13,6 +15,18 @@ def index():
         total += random_number
         numbers.append(random_number)
     return render_template('index.html', dice_count = dice_count, numbers = numbers, total = total)
+
+@app.route('/api/v1/resources/rannad/all', methods = ['GET'])
+def api_all():
+    dbclient = get_database()
+    collections = dbclient['rand']
+    return jsonify(dumps(collections.find()))
+
+@app.route('/api/v1/resources/rannad/<nimi>', methods = ['GET'])
+def api_get_rand(nimi):
+    dbclient = get_database()
+    collections = dbclient['rand']
+    return jsonify(dumps(collections.find({'nimi': nimi})))
 
 if __name__ == '__main__':
     app.run(debug = True)
---
+++ db.py
@@ -0,0 +1,39 @@
+from pymongo import MongoClient
+from dateutil.parser import parse
+
+def get_database():
+    CONNECT_STRING = 'mongodb+srv://dbuser:dbuser@cluster0.hy6bk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    client = MongoClient(CONNECT_STRING)
+    return client['python202105-web']
+
+if __name__ == '__main__':
+    with open('rannailm.csv', encoding = 'UTF-8') as file:
+        dbclient = get_database()
+        collection = dbclient['rand']
+        i = 0
+        for row in file:
+            if i > 0:
+                cells = row.strip().split(';')
+                existing = collection.find_one({'nimi': cells[0]})
+                if existing is None:
+                    rand = {
+                        'nimi': cells[0],
+                        'kuupaev': parse(cells[1] + ' ' + cells[2]),
+                        'ohk': int(cells[3]),
+                        'vesi': int(cells[4]),
+                        'rahvaarv': int(cells[5])
+                    }
+                    collection.insert_one(rand)
+                    print(rand)
+                else:
+                    collection.update_one(
+                        {'nimi': cells[0]},
+                        {'$set': {
+                            'kuupaev': parse(cells[1] + ' ' + cells[2]),
+                            'ohk': int(cells[3]),
+                            'vesi': int(cells[4]),
+                            'rahvaarv': int(cells[5])
+                        }}
+                    )
+            i += 1
+        file.close()
